449ac7490844efcd6e3fab36276b48ae5ca41c86
tushar2k <tusharmittal2502@gmail.com>
2021-05-10 02:59:17 +0530

    add dynamic notes via an array of note objects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-function App(){
-    return (<div>
-    <Header />
-    <Note />
-    <Footer />
-    </div>)
-};
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((thisNote) => 
+        <Note key={thisNote.key} title={thisNote.title} content={thisNote.content} />
+      )}
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-function Footer(){
-    const d = new Date().getFullYear();
-    return <footer><p>Copyright ⓒ {d}</p></footer>
-};
+function Footer() {
+  const d = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {d}</p>
+    </footer>
+  );
+}
 
 export default Footer;
