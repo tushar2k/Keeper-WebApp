4be53871de71479749dfea45533c0013666f198e
tushar2k <tusharmittal2502@gmail.com>
2021-05-12 00:05:36 +0530

    Ability to add notes-Basic structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map((thisNote) => 
-        <Note key={thisNote.key} title={thisNote.title} content={thisNote.content} />
-      )}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
